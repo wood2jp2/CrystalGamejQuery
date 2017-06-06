0c795fc80f894a57b0451c3997048584e15083f5
Josh Wood <wood2jp2@gmail.com>
2017-06-05 21:50:43 -0400

    minor styling changes, added gem values to track for user

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,6 +17,10 @@ var runGame = function() {
 					gem4 = (Math.floor(Math.random()*11+2));
 					$('.playerScore').text(playerScore);
 					$('.computerPoints').text(computerPoints);
+					$('#gem1value').text("?");
+					$('#gem2value').text("?");
+					$('#gem3value').text("?");
+					$('#gem4value').text("?");
 				};
 								
 				var scoreCheck = function() {
@@ -38,24 +42,28 @@ var runGame = function() {
 				$('.gem1').on('click', function() {
 					playerScore+=gem1;
 					$('.playerScore').text(playerScore);
+					$('#gem1value').text(gem1);
 					scoreCheck();
 				});
 
 				$('.gem2').on('click', function() {
 					playerScore+=gem2;
 					$('.playerScore').text(playerScore);
+					$('#gem2value').text(gem2);
 					scoreCheck();
 				});
 
 				$('.gem3').on('click', function() {
 					playerScore+=gem3;
 					$('.playerScore').text(playerScore);
+					$('#gem3value').text(gem3);
 					scoreCheck();
 				});
 
 				$('.gem4').on('click', function() {
 					playerScore+=gem4;
 					$('.playerScore').text(playerScore);
+					$('#gem4value').text(gem4);
 					scoreCheck();
 				});
 }
